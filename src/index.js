a8186675d1ca880dd5c5b370cacb60f3a452d78b
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-08-29 14:54:54 +0100

    Added Form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+
+const form = document.querySelector("form");
+const newTask = document.querySelector("#newTask");
+console.log(newTask)
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  const taskData = {};
+  for (const [name, value] of data.entries()) {
+    taskData[name] = value;
+  }
+}
+)
+
+function openForm() {
+    document.getElementById("formContainer").classList.remove("notdisplayed");
+    document.getElementById("formContainer").classList.add("displayed");
+  }
+  
+  function closeForm() {
+    document.getElementById("formContainer").classList.add("notdisplayed");
+    document.getElementById("formContainer").classList.remove("displayed");
+  }
+
+newTask.addEventListener("click",()=>{
+    openForm()
+})
