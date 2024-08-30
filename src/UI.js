643f9a08ec2170bd1e81aca42f6e4c1ee16152ce
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-08-30 21:30:37 +0100

    Fixed UI Control

--- src/UI.js
+++ src/UI.js
@@ -21,11 +21,11 @@ export function updateTaskUI(){
                 <div class="info">
                     <h2>${element.name}</h2>
                     <h3>${element.importance}</h3>
-                    <p>${element.note}</p>
+                    <p>Note: ${element.note}</p>
                     <p>Due: ${element.dueDate}</p>
                 </div>
                 <button class="remove"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
             </div>`
-        mainContainer.innerHTML += card
+        mainContainer.innerHTML += card;
     });
 }
