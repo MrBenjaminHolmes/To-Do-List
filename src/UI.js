e8cc2fba017eaf282d4c9c4b97746b470143ffe0
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-09-01 19:16:19 +0100

    Updated UI

--- src/UI.js
+++ src/UI.js
@@ -21,7 +21,7 @@ export function addTasks(projectName) {
     (element) => element.name === projectName
   );
   if (project) {
-    mainContainer.innerHTML = `<h2>${project.name}</h2>`;
+    mainContainer.innerHTML = `<h2 class="boardHeading" >${project.name}</h2>`;
     project.tasks.forEach((task, index) => {
       const card = `
                 <div class="card">
@@ -29,7 +29,7 @@ export function addTasks(projectName) {
                         <h2>${task.name}</h2>
                         <h3>${task.importance}</h3>
                         <p>Note: ${task.note}</p>
-                        <p>Due: ${task.dueDate}</p>
+                        <p class="due" >Due: ${task.dueDate}</p>
                     </div>
                     <button class="remove" data-index="${index}"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
                 </div>`;
